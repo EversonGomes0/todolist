4fecd731f05372eb953fa74605c2328f363426df
Everson Ramos Gomes <eversonramos2015@gmail.com>
2025-12-13 16:35:26 -0300

    Utilizando ResponseEntity, usado para customizar as mensagem de sucesse e erro

--- src/main/java/br/com/everson/todolist/user/UserController.java
+++ src/main/java/br/com/everson/todolist/user/UserController.java
@@ -3,7 +3,10 @@ package br.com.everson.todolist.user;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 
 
@@ -15,14 +18,15 @@ public class UserController {
     private IUserRepository userRepository;
     
     @PostMapping("/")
-    public UserModel create(@RequestBody UserModel userModel){
+    public ResponseEntity create(@RequestBody UserModel userModel){
         UserModel user =this.userRepository.findByUserName(userModel.getUserName());
         if(user != null){
             System.out.println("User already exists!");
-            throw new RuntimeException("User already exists!");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("User already exists!");
         }
+        
         UserModel userCreated = this.userRepository.save(userModel);
-        return userCreated;
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
         
     }
 }
