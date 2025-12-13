ece0e1f08b6a702f46d9ce99be5b66ef0a750536
Everson Ramos Gomes <eversonramos2015@gmail.com>
2025-12-13 16:48:07 -0300

    Criptografando a senha do usuário, utilizando a biblioteca Bcrypt

--- src/main/java/br/com/everson/todolist/user/UserController.java
+++ src/main/java/br/com/everson/todolist/user/UserController.java
@@ -3,7 +3,9 @@ package br.com.everson.todolist.user;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.apache.catalina.connector.Response;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -24,6 +26,11 @@ public class UserController {
             System.out.println("User already exists!");
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("User already exists!");
         }
+
+        var passwordHashred = BCrypt.withDefaults()
+        .hashToString(12, userModel.getPassword().toCharArray());
+
+        userModel.setPassword(passwordHashred);
         
         UserModel userCreated = this.userRepository.save(userModel);
         return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
